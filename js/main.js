39f65fe2b11112fc37d9627833f0d17a52ba528d
Sophia Derugen-Toomey <windmillwarrior@gmail.com>
2017-11-20 19:04:24 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -4,19 +4,28 @@ console.log ("Up and running!");
 
 var cards = ["queen", "king", "queen", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+var checkForMatch = function () {
+if (ardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
 }
+}
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cardId);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(1);
+//	if (cardsInPlay.length === 2) {
+//	if (cardsInPlay[0] === cardsInPlay[1]) {
+//		alert("You found a match!");
+//	} else {
+//		alert("Sorry, try again.");
+//	}
+//}
+
 
 
 
