c68e40a3419ae1c82a486807f8f4645ef9cde709
Sophia Derugen-Toomey <windmillwarrior@gmail.com>
2017-11-22 12:01:42 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,22 +2,45 @@ console.log ("Up and running!");
 // console.log ("User flipped " + cardOne);
 // console.log ("User flipped " + cardFour);
 
-var cards = ["queen", "king", "queen", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function () {
-if (ardsInPlay[0] === cardsInPlay[1]) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
 }
 }
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cardId);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 }
 flipCard(0);
-flipCard(1);
+flipCard(2);
 //	if (cardsInPlay.length === 2) {
 //	if (cardsInPlay[0] === cardsInPlay[1]) {
 //		alert("You found a match!");
