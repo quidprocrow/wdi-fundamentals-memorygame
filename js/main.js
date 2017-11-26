18ba47336c8a7e752bf66abde84fdcc3bd9d2eb9
Sophia Derugen-Toomey <windmillwarrior@gmail.com>
2017-11-25 19:22:41 -0500

    created game logic;

--- js/main.js
+++ js/main.js
@@ -26,28 +26,33 @@ var cards = [
 ];
 var cardsInPlay = [];
 var checkForMatch = function () {
+this.setAttribute('src', cards[cardId].cardImage);	
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
 }
 }
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 }
-flipCard(0);
-flipCard(2);
-//	if (cardsInPlay.length === 2) {
-//	if (cardsInPlay[0] === cardsInPlay[1]) {
-//		alert("You found a match!");
-//	} else {
-//		alert("Sorry, try again.");
-//	}
-//}
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+createBoard();
+
 
 
 
